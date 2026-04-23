f87565e5f8cbc91fe2b9ee6f87537c67a0d2fb42
Albert Portas <albertportasavelli@gmail.com>
2026-04-23 19:23:31 +0200

    chore: add Spring Boot project base setup

---
+++ src/main/java/com/inpusduat/inpusduat/InpusduatApplication.java
@@ -0,0 +1,13 @@
+package com.inpusduat.inpusduat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpusduatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InpusduatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inpusduat/inpusduat/InpusduatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inpusduat.inpusduat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InpusduatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
